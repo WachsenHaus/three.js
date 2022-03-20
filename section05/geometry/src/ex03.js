08e1f690037f0be52f041d7cc743f003a4d088c2
wachsenhaus <bi9choi@gmail.com>
2022-03-20 22:18:12 +0900

    <추가> : <사인을 이용한 지오메트리>

---
+++ section05/geometry/src/ex03.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// ----- 주제: Geometry 기본
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 10;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Mesh
+  const geometry = new THREE.SphereGeometry(5, 64, 64);
+  const material = new THREE.MeshStandardMaterial({
+    color: 'hotpink',
+    side: THREE.DoubleSide,
+    // wireframe: true,
+    flatShading: true,
+  });
+  //   material.
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  //vertex는
+  const positionArray = geometry.attributes.position.array;
+  //   const
+  for (let i = 0; i < positionArray.length; i += 3) {
+    // 정점(vertex) 한개의 x,y,z좌표를 랜덤으로 조정한다.
+    positionArray[i] += (Math.random() - 0.5) * 0.2; //0.5를 뺴는것은 값의 분포를 골고루 하기위해서이다. math.random은 0이상 1이하이다.
+    positionArray[i + 1] += (Math.random() - 0.5) * 0.2;
+    positionArray[i + 2] += (Math.random() - 0.5) * 0.2;
+  }
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+    const time = clock.getElapsedTime();
+    console.log(time);
+
+    for (let i = 0; i < positionArray.length; i += 3) {
+      // 정점(vertex) 한개의 x,y,z좌표를 랜덤으로 조정한다.
+      positionArray[i] += Math.sin(time) * 0.01;
+      positionArray[i + 1] += Math.sin(time) * 0.01;
+      positionArray[i + 2] += Math.sin(time) * 0.01;
+      //   positionArray[i + 1] += (Math.random() - 0.5) * 0.2;
+      //   positionArray[i + 2] += (Math.random() - 0.5) * 0.2;
+    }
+
+    geometry.attributes.position.needsUpdate = true;
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
