bb0c12015e18d5959378fb625544ef027353a28e
Younghoon CHOI _^ <bi9choi@gmail.com>
2022-06-07 13:57:33 +0900

    <추가> : <three>

---
+++ section06/controls/src/ex05.js
@@ -0,0 +1,110 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { FlyControls } from 'three/examples/jsm/controls/FlyControls';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new PointerLockControls(camera, renderer.domElement);
+  console.log(controls.domElement === renderer.domElement);
+  controls.domElement.addEventListener('click', () => {
+    controls.lock();
+  });
+
+  controls.addEventListener('lock', () => {
+    console.log('lock');
+  });
+  controls.addEventListener('unlock', () => {
+    console.log('unlock');
+  });
+  // controls.lock();
+  // 댐핑을 사용하면 카메라가 회전할때 부드러워진다. draw할때마다 update를 해야함.
+  // controls.enableDamping = true;
+  // controls.maxDistance = 20;
+  // controls.minDistance = 2;
+  // controls.rollSpeed = 0.1;
+  // controls.dragToLook = true;
+  // controls.minPolarAngle = THREE.MathUtils.degToRad(45);
+  // controls.maxPolarAngle = THREE.MathUtils.degToRad(135);
+  // controls.autoRotate = true;
+  // controls.autoRotateSpeed = 50;
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  //   const material = new THREE.MeshStandardMaterial({
+  // color: 'seagreen',
+  //   });
+  //   const mesh = new THREE.Mesh(geometry, material);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(
+			${50 + Math.floor(Math.random() * 205)},
+			${50 + Math.floor(Math.random() * 205)},
+			${50 + Math.floor(Math.random() * 205)}
+			)`,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+    scene.add(mesh);
+  }
+  //   scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    // controls.update(delta);
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
